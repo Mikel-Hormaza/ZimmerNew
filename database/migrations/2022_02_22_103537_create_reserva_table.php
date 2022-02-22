626e9c12fa9d79f94469d16c11e365779a198286
Mikel-Hormaza <73583125+Mikel-Hormaza@users.noreply.github.com>
2022-02-22 11:41:19 +0100

    migracion

---
+++ database/migrations/2022_02_22_103537_create_reserva_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->references("id")->on("users")->onDelete('cascade');;
+            $table->integer("hotel_id");
+            $table->date("fecha_entrada");
+            $table->date("fecha_salida");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reserva');
+    }
+}
